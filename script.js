060774833f43bf35d017bdab319c4a50b230080c
karthikeyan <karthikeyan76652@gmail.com>
2025-07-13 20:10:43 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function createCounter() {
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const counter = createCounter();
+
+const button = document.getElementById("clickBtn");
+const display = document.getElementById("clickCount");
+
+button.addEventListener("click", () => {
+  const newCount = counter();
+  display.textContent = newCount;
+});
